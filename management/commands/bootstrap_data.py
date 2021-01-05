c7e41b7d2e7e3aedf47a7100fe2e0523a606f518
Kevin Clark <kevinwilliamclark@gmail.com>
2021-01-05 12:19:48 -0600

    adding database recipe commands and working on image upload

---
+++ management/commands/bootstrap_data.py
@@ -0,0 +1,188 @@
+from django.core.management.base import BaseCommand
+from recipe_app.models import Recipe
+from recipe_user.models import Author
+
+
+class Command(BaseCommand):
+    help = "will create some recipes and authors"
+
+    def handle(self, *args, **options):
+
+        Author.objects.bulk_create(
+            [
+                Author(
+                    name="Jim",
+                    bio="Lorem ipsum dolor sit amet."
+                    ),
+                Author(
+                    name="Sandy",
+                    bio="Lorem ipsum dolor sit amet."
+                    ),
+                Author(
+                    name="Kelly",
+                    bio="Lorem ipsum dolor sit amet."
+                    ),
+                Author(
+                    name="Ben",
+                    bio="Lorem ipsum dolor sit amet."
+                    ),
+                Author(
+                    name="Randy",
+                    bio="Lorem ipsum dolor sit amet."
+                    ),
+                Author(
+                    name="Carey",
+                    bio="Lorem ipsum dolor sit amet."
+                    ),
+                Author(
+                    name="Jacob",
+                    bio="Lorem ipsum dolor sit amet."
+                    ),
+                Author(
+                    name="Sam",
+                    bio="Lorem ipsum dolor sit amet."
+                    ),
+                Author(
+                    name="Jill",
+                    bio="Lorem ipsum dolor sit amet."
+                    ),
+                Author(
+                    name="Jennifer",
+                    bio="Lorem ipsum dolor sit amet."
+                    )
+            ]
+        )
+        Recipe.objects.bulk_create(
+            [
+                Recipe(
+                    title="Tacos",
+                    author="Jill",
+                    description="These tacos are the best",
+                    items="taco shells. beef. lettuce. salsa. onions.",
+                    timerequired="""Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa
+                                    qui officia deserunt mollit anim id est
+                                    laborum.""",
+                    instruction="""Duis aute irure dolor in reprehenderit
+                                    in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur.""",
+                    ),
+                Recipe(
+                    title="Pasta",
+                    author="Sam",
+                    description="This pasta will blow your socks off.",
+                    items="sauce. meatballs. cheese. pasta.",
+                    timerequired="""Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa
+                                    qui officia deserunt mollit anim id est
+                                    laborum.""",
+                    instruction="""Duis aute irure dolor in reprehenderit
+                                    in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur.""",
+                    ),
+                Recipe(
+                    title="Sushi",
+                    author="Jacob",
+                    description="Amazing sushi with wasabi",
+                    items="rice. fish. wasabi. cucumber. seaweed.",
+                    timerequired="""Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa
+                                    qui officia deserunt mollit anim id est
+                                    laborum.""",
+                    instruction="""Duis aute irure dolor in reprehenderit
+                                    in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur.""",
+                    ),
+                Recipe(
+                    title="Fish with salad",
+                    author="Sam",
+                    description="Grilled fish like no other",
+                    items="Fish. onion. lettuce. tomato. cucumber. ranch dressing.",
+                    timerequired="""Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa
+                                    qui officia deserunt mollit anim id est
+                                    laborum.""",
+                    instruction="""Duis aute irure dolor in reprehenderit
+                                    in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur.""",
+                    ),
+                Recipe(
+                    title="Pizza",
+                    author="Kelly",
+                    description="Pizza to die for with pepperoni",
+                    items="Crust. cheese. sauce. pepperoni. olives.",
+                    timerequired="""Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa
+                                    qui officia deserunt mollit anim id est
+                                    laborum.""",
+                    instruction="""Duis aute irure dolor in reprehenderit
+                                    in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur.""",
+                    ),
+                Recipe(
+                    title="Hamburger",
+                    author="Jim",
+                    description="Juicy burger with cheese",
+                    items="Buns. hamburger. cheese. lettuce. pickles.",
+                    timerequired="""Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa
+                                    qui officia deserunt mollit anim id est
+                                    laborum.""",
+                    instruction="""Duis aute irure dolor in reprehenderit
+                                    in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur.""",
+                    ),
+                Recipe(
+                    title="BBQ Chicken",
+                    author="Kelly",
+                    description="Never ever have you tried chicken like this.",
+                    items="BBQ sauce. chicken wings. ",
+                    timerequired="""Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa
+                                    qui officia deserunt mollit anim id est
+                                    laborum.""",
+                    instruction="""Duis aute irure dolor in reprehenderit
+                                    in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur.""",
+                    ),
+                Recipe(
+                    title="Grilled Tofu",
+                    author="Sandy",
+                    description="Never say never until you try this Tofu.",
+                    items="Tofu, soy sauce. rice.",
+                    timerequired="""Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa
+                                    qui officia deserunt mollit anim id est
+                                    laborum.""",
+                    instruction="""Duis aute irure dolor in reprehenderit
+                                    in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur.""",
+                    ),
+                Recipe(
+                    title="Omelet",
+                    author="Sandy",
+                    description="Perfect breakfast to wake up to",
+                    items="Eggs. bell pepper. onion. cilantro. sour cream.",
+                    timerequired="""Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa
+                                    qui officia deserunt mollit anim id est
+                                    laborum.""",
+                    instruction="""Duis aute irure dolor in reprehenderit
+                                    in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur.""",
+                    ),
+                Recipe(
+                    title="Cake",
+                    author="Jim",
+                    description="Jims Cake is for every birthday, and more.",
+                    items="Flour. sugar. butter. vanilla. baking powder. raspberry jam.",
+                    timerequired="""Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa
+                                    qui officia deserunt mollit anim id est
+                                    laborum.""",
+                    instruction="""Duis aute irure dolor in reprehenderit
+                                    in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur.""",
+                    )
+            ]
+        )
--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import User
-from django.utils import timezone
+# from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import User
+# from django.utils import timezone
 from recipe_user.models import Author
 
 
@@ -9,12 +9,10 @@ class Recipe(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     description = models.TextField()
-    # items = models.TextField()
+    items = models.TextField()
     timerequired = models.CharField(max_length=100)
     instructions = models.TextField()
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
 
     def __str__(self):
         return f"{self.title} - {self.author}"
-
-
-    
