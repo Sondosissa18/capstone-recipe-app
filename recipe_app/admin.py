d588f96d669df4406073561e2ae1935353b33797
Kevin Clark <kevinwilliamclark@gmail.com>
2021-01-08 18:05:35 -0600

    add .env file and hides the secret key

--- recipe_app/admin.py
+++ recipe_app/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-# from .models import Author, Recipe
-# class CustomUserAdmin(UserAdmin):
-#     model = Author
-
-# admin.site.register(Author, CustomUserAdmin)
-
-
 from recipe_app.models import Recipe
 
 
 admin.site.register(Recipe)
-
-
