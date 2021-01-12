bd19fce0ea543c7a60cf46470b5fc5078af0e444
Sondos Issa <sondosissa@Sondoss-iMac.local>
2021-01-12 11:07:48 -0600

    fixed issues

--- notification/forms.py
+++ notification/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class AddMessageForm(forms.Form):
-    text = forms.CharField(max_length=140)
+    # text = forms.CharField(max_length=140)
+    text = forms.CharField(widget=forms.Textarea)
