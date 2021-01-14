44df92d8adbabfb598d7a75fb690553a28f8b0dd
Kevin Clark <kevinwilliamclark@gmail.com>
2021-01-13 19:05:39 -0600

    adds three random recipes to the homepage

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -7,6 +7,7 @@ from recipe_user.models import Message
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from authentication.forms import LoginForm, SignupForm
+import random
 
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.views import LogoutView
@@ -16,21 +17,6 @@ from .forms import AddRecipeForm, AddMessageForm
 from django.conf import settings
 from django.utils.decorators import method_decorator
 
-# def login_view(request):
-#     if request.method == "POST":
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             data = form.cleaned_data
-#             user = authenticate(
-#                 request, username=data["username"], password=data["password"]
-#             )
-#             if user:
-#                 login(request, user)
-#                 return HttpResponseRedirect(
-#                     request.GET.get("next", reverse("home"))
-#                 )
-#     form = LoginForm()
-#     return render(request, "login.html", {"form": form})
 
 # @login_required()
 # def recipe_detail_view(request, recipe_id):
@@ -45,77 +31,26 @@ class RecipeDetailView(View):
                       {"recipes": my_recipe})
 
 
-# def following_view(request, user_id):
-#     if request.user.id == user_id:
-#         # you cant follow yourself
-#         return HttpResponseRedirect(reverse("home"))
-#     follows = Author.objects.get(id=user_id)
-
-#     allfollowers = request.user.following.all()
-
-#     if follows not in allfollowers:
-#         request.user.following.add(follows)
-#     else:
-#         request.user.following.remove(follows)
-#     return HttpResponseRedirect(reverse("home"))
-
-# 22222
 # @login_required(login_url="/login")
 def index_view(request):
     form = LoginForm()
     signup_form = SignupForm()
+    recipes = Recipe.objects.all()
+    how_many = recipes.count()
+    one = random.randint(1, how_many)
+    two = random.randint(1, how_many)
+    three = random.randint(1, how_many)
+    one_recipe = Recipe.objects.get(id=one)
+    two_recipe = Recipe.objects.get(id=two)
+    three_recipe = Recipe.objects.get(id=three)
     return render(
         request, "home.html", {
-                "recipes": Recipe.objects.all(),
-                "message": Message.objects.all(),
+                "one_recipe": one_recipe,
+                "two_recipe": two_recipe,
+                "three_recipe": three_recipe,
                 "form": form,
                 "signup_form": signup_form
             })
-
-
-
-# class IndexView(View):
-#     def get(self, request):
-#         return render(request, "home.html", 
-#                       {"recipes": Recipe.objects.all(),
-#                        "message": Message.objects.all()})
-
-
-# @login_required
-# def recipe_detail_view(request):
-#     html = "generic_form.html"
-#     if request.method == "POST":
-#         form = AddRecipeForm(request.POST)
-#         if form.is_valid():
-#             data = form.cleaned_data
-#             new_recipe = Recipe.objects.create(
-#                 title=data['title'],
-#                 description=data['description'],
-#                 author=data['author']
-#             )
-#             return HttpResponseRedirect(reverse("homepage"))
-        
-#     form = AddRecipeForm()
-#     return render(request, html, {'form': form})
-
-
-# ///////seeems not correct 
-# @login_required
-# def message_view(request):
-#     html = "generic_form.html"
-#     if request.method == "POST":
-#         form = AddMessageForm(request.POST)
-#         if form.is_valid():
-#             data = form.cleaned_data
-#             Message.objects.create(
-#                 text=data['text'],
-#                 created_at=data['created_at'],
-#                 author=data['author']
-#             )
-#             return HttpResponseRedirect(reverse("homepage"))
-
-#     form = AddMessageForm()
-#     return render(request, html, {'form': form})
 
 
 def about_view(request):
@@ -137,11 +72,9 @@ class SearchBar(LoginRequiredMixin, View):
         search = request.GET.get('search')
         post = Recipe.objects.all().filter(title=search)
         return render(request, html, {'post': post})
-           
 
 
-
-# help from Matt with this request.FILES upload. 
+# help from Matt with this request.FILES upload
 def recipe_upload(request):
     if request.method == "POST":
         form = AddRecipeForm(request.POST, request.FILES)
